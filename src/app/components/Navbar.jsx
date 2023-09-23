5bab6d3c78039591ccc6728a19a4e3b591a4db2a
Christian Chung <christianchung11@gmail.com>
2023-09-22 20:07:30 -0500

    bug fix heroicons

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react'
 import Link from 'next/link'
 import NavLink from './NavLink'
-import { Bars3Icon, XmarkIcon } from "@heroicons/react/24/solid";
+// import { Bars3Icon, XmarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from './MenuOverlay';
 
 const navLinks = [
@@ -52,7 +52,7 @@ const Navbar = () => {
                 </ul>
             </div> */}
         </div>
-        {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
+        {/* {navbarOpen ? <MenuOverlay links={navLinks} /> : null} */}
     </nav>
   )
 }
